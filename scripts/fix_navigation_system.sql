f7195b72ca1edf73884f692cb3d3c2dd12546c79
darian7b7 <daryannabo16@gmail.com>
2025-04-27 09:59:52 +0200

    ✅ updated something

--- scripts/fix_navigation_system.sql
+++
@@ -1,9 +0,0 @@
--- Convert boolean true values to 'built-in' string
-UPDATE "VehicleDetails"
-SET "navigationSystem" = 'built-in'
-WHERE "navigationSystem" = 'true' OR "navigationSystem" IS TRUE;
-
--- Convert boolean false values to 'none' string
-UPDATE "VehicleDetails"
-SET "navigationSystem" = 'none'
-WHERE "navigationSystem" = 'false' OR "navigationSystem" IS FALSE;
